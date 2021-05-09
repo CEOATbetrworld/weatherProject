445e7b02bcf5462e559d7938944e5f95bfb8ee7a
DHRUV KUMAR <dkiphones620@gmail.com>
2021-05-09 17:37:02 +0530

    added weather description and data

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const app = express();
 
 app.get("/", function (req, res) {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=jaipur&appid=2684623b2d7293f45107711aa21febb4"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=jaipur&appid=2684623b2d7293f45107711aa21febb4&units=metric"
 
     https.get(url, function (response) {
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in Jaipur is " + temp + " degrees Celcius.</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+        })
     })
 
-    res.send("server is running");
+
 })
 
 app.listen(3000, function () {
